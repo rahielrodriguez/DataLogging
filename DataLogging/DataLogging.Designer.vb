79d133e983fe94abfacbcd8609f34101ad05fe04
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-11-04 18:40:17 -0700

    Attempt to track qy board analog output Need to update refresh rate and data log

--- DataLogging/DataLogging.Designer.vb
+++ DataLogging/DataLogging.Designer.vb
@@ -23,92 +23,50 @@ Partial Class DataLogging
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.TopMenuStrip = New System.Windows.Forms.MenuStrip()
-        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.StopToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.InfoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
-        Me.StartButton = New System.Windows.Forms.Button()
+        Me.ContextMenuStrip = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.LogButton = New System.Windows.Forms.Button()
         Me.StopButton = New System.Windows.Forms.Button()
         Me.SaveButton = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ComButton = New System.Windows.Forms.Button()
+        Me.LogPictureBox = New System.Windows.Forms.PictureBox()
         Me.SerialPort = New System.IO.Ports.SerialPort(Me.components)
         Me.SampleRateLabel = New System.Windows.Forms.Label()
-        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.SampleRateTrackBar = New System.Windows.Forms.TrackBar()
         Me.MinSampleLabel = New System.Windows.Forms.Label()
         Me.MaxSampleLabel = New System.Windows.Forms.Label()
-        Me.TopMenuStrip.SuspendLayout()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.ToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FileStatusLabel = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.CurrentSampleRateLabel = New System.Windows.Forms.Label()
+        Me.PortComboBox = New System.Windows.Forms.ComboBox()
         Me.GroupBox1.SuspendLayout()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LogPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SampleRateTrackBar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TopMenuStrip
+        'ContextMenuStrip
         '
-        Me.TopMenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
-        Me.TopMenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem, Me.HelpToolStripMenuItem})
-        Me.TopMenuStrip.Location = New System.Drawing.Point(0, 0)
-        Me.TopMenuStrip.Name = "TopMenuStrip"
-        Me.TopMenuStrip.Size = New System.Drawing.Size(800, 28)
-        Me.TopMenuStrip.TabIndex = 0
-        Me.TopMenuStrip.Text = "MenuStrip1"
+        Me.ContextMenuStrip.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ContextMenuStrip.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip.Size = New System.Drawing.Size(61, 4)
         '
-        'MenuToolStripMenuItem
+        'LogButton
         '
-        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.StopToolStripMenuItem, Me.SaveToolStripMenuItem})
-        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
-        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(60, 24)
-        Me.MenuToolStripMenuItem.Text = "Menu"
-        '
-        'StartToolStripMenuItem
-        '
-        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
-        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
-        Me.StartToolStripMenuItem.Text = "Start"
-        '
-        'StopToolStripMenuItem
-        '
-        Me.StopToolStripMenuItem.Name = "StopToolStripMenuItem"
-        Me.StopToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
-        Me.StopToolStripMenuItem.Text = "Stop"
-        '
-        'SaveToolStripMenuItem
-        '
-        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
-        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(123, 26)
-        Me.SaveToolStripMenuItem.Text = "Save"
-        '
-        'HelpToolStripMenuItem
-        '
-        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InfoToolStripMenuItem})
-        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
-        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(55, 24)
-        Me.HelpToolStripMenuItem.Text = "Help"
-        '
-        'InfoToolStripMenuItem
-        '
-        Me.InfoToolStripMenuItem.Name = "InfoToolStripMenuItem"
-        Me.InfoToolStripMenuItem.Size = New System.Drawing.Size(133, 26)
-        Me.InfoToolStripMenuItem.Text = "About"
-        '
-        'ContextMenuStrip1
-        '
-        Me.ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
-        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
-        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
-        '
-        'StartButton
-        '
-        Me.StartButton.Location = New System.Drawing.Point(6, 21)
-        Me.StartButton.Name = "StartButton"
-        Me.StartButton.Size = New System.Drawing.Size(140, 72)
-        Me.StartButton.TabIndex = 2
-        Me.StartButton.Text = "Start"
-        Me.StartButton.UseVisualStyleBackColor = True
+        Me.LogButton.Location = New System.Drawing.Point(6, 21)
+        Me.LogButton.Name = "LogButton"
+        Me.LogButton.Size = New System.Drawing.Size(140, 72)
+        Me.LogButton.TabIndex = 2
+        Me.LogButton.Text = "Log"
+        Me.LogButton.UseVisualStyleBackColor = True
         '
         'StopButton
         '
@@ -131,47 +89,57 @@ Partial Class DataLogging
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.ComButton)
         Me.GroupBox1.Controls.Add(Me.SaveButton)
-        Me.GroupBox1.Controls.Add(Me.StartButton)
+        Me.GroupBox1.Controls.Add(Me.LogButton)
         Me.GroupBox1.Controls.Add(Me.StopButton)
-        Me.GroupBox1.Location = New System.Drawing.Point(340, 328)
+        Me.GroupBox1.Location = New System.Drawing.Point(351, 359)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(448, 110)
+        Me.GroupBox1.Size = New System.Drawing.Size(591, 110)
         Me.GroupBox1.TabIndex = 5
         Me.GroupBox1.TabStop = False
         '
-        'PictureBox1
+        'ComButton
         '
-        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.PictureBox1.Location = New System.Drawing.Point(0, 34)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(800, 288)
-        Me.PictureBox1.TabIndex = 6
-        Me.PictureBox1.TabStop = False
+        Me.ComButton.Location = New System.Drawing.Point(444, 21)
+        Me.ComButton.Name = "ComButton"
+        Me.ComButton.Size = New System.Drawing.Size(140, 72)
+        Me.ComButton.TabIndex = 5
+        Me.ComButton.Text = "&Com"
+        Me.ComButton.UseVisualStyleBackColor = True
+        '
+        'LogPictureBox
+        '
+        Me.LogPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.LogPictureBox.Location = New System.Drawing.Point(0, 28)
+        Me.LogPictureBox.Name = "LogPictureBox"
+        Me.LogPictureBox.Size = New System.Drawing.Size(942, 288)
+        Me.LogPictureBox.TabIndex = 6
+        Me.LogPictureBox.TabStop = False
         '
         'SampleRateLabel
         '
         Me.SampleRateLabel.AutoSize = True
-        Me.SampleRateLabel.Location = New System.Drawing.Point(120, 346)
+        Me.SampleRateLabel.Location = New System.Drawing.Point(116, 18)
         Me.SampleRateLabel.Name = "SampleRateLabel"
         Me.SampleRateLabel.Size = New System.Drawing.Size(86, 16)
         Me.SampleRateLabel.TabIndex = 8
         Me.SampleRateLabel.Text = "Sample Rate"
         '
-        'TrackBar1
+        'SampleRateTrackBar
         '
-        Me.TrackBar1.Location = New System.Drawing.Point(15, 365)
-        Me.TrackBar1.Maximum = 100
-        Me.TrackBar1.Minimum = 1
-        Me.TrackBar1.Name = "TrackBar1"
-        Me.TrackBar1.Size = New System.Drawing.Size(319, 56)
-        Me.TrackBar1.TabIndex = 9
-        Me.TrackBar1.Value = 1
+        Me.SampleRateTrackBar.Location = New System.Drawing.Point(6, 37)
+        Me.SampleRateTrackBar.Maximum = 100
+        Me.SampleRateTrackBar.Minimum = 1
+        Me.SampleRateTrackBar.Name = "SampleRateTrackBar"
+        Me.SampleRateTrackBar.Size = New System.Drawing.Size(319, 56)
+        Me.SampleRateTrackBar.TabIndex = 9
+        Me.SampleRateTrackBar.Value = 10
         '
         'MinSampleLabel
         '
         Me.MinSampleLabel.AutoSize = True
-        Me.MinSampleLabel.Location = New System.Drawing.Point(12, 346)
+        Me.MinSampleLabel.Location = New System.Drawing.Point(3, 18)
         Me.MinSampleLabel.Name = "MinSampleLabel"
         Me.MinSampleLabel.Size = New System.Drawing.Size(73, 16)
         Me.MinSampleLabel.TabIndex = 10
@@ -180,56 +148,140 @@ Partial Class DataLogging
         'MaxSampleLabel
         '
         Me.MaxSampleLabel.AutoSize = True
-        Me.MaxSampleLabel.Location = New System.Drawing.Point(240, 346)
+        Me.MaxSampleLabel.Location = New System.Drawing.Point(231, 18)
         Me.MaxSampleLabel.Name = "MaxSampleLabel"
         Me.MaxSampleLabel.Size = New System.Drawing.Size(94, 16)
         Me.MaxSampleLabel.TabIndex = 11
         Me.MaxSampleLabel.Text = "100 samples/s"
         '
+        'Timer
+        '
+        Me.Timer.Interval = 10
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.SettingsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(942, 28)
+        Me.MenuStrip1.TabIndex = 12
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveToolStripMenuItem, Me.OpenFileToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(46, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(155, 26)
+        Me.SaveToolStripMenuItem.Text = "&Save"
+        '
+        'OpenFileToolStripMenuItem
+        '
+        Me.OpenFileToolStripMenuItem.Name = "OpenFileToolStripMenuItem"
+        Me.OpenFileToolStripMenuItem.Size = New System.Drawing.Size(155, 26)
+        Me.OpenFileToolStripMenuItem.Text = "&Open File"
+        '
+        'SettingsToolStripMenuItem
+        '
+        Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(76, 24)
+        Me.SettingsToolStripMenuItem.Text = "Settings"
+        '
+        'FileStatusLabel
+        '
+        Me.FileStatusLabel.AutoEllipsis = True
+        Me.FileStatusLabel.AutoSize = True
+        Me.FileStatusLabel.Location = New System.Drawing.Point(12, 449)
+        Me.FileStatusLabel.Name = "FileStatusLabel"
+        Me.FileStatusLabel.Size = New System.Drawing.Size(48, 16)
+        Me.FileStatusLabel.TabIndex = 13
+        Me.FileStatusLabel.Text = "Label1"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.CurrentSampleRateLabel)
+        Me.GroupBox2.Controls.Add(Me.MaxSampleLabel)
+        Me.GroupBox2.Controls.Add(Me.MinSampleLabel)
+        Me.GroupBox2.Controls.Add(Me.SampleRateTrackBar)
+        Me.GroupBox2.Controls.Add(Me.SampleRateLabel)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 322)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(333, 110)
+        Me.GroupBox2.TabIndex = 14
+        Me.GroupBox2.TabStop = False
+        '
+        'CurrentSampleRateLabel
+        '
+        Me.CurrentSampleRateLabel.AutoSize = True
+        Me.CurrentSampleRateLabel.Location = New System.Drawing.Point(99, 77)
+        Me.CurrentSampleRateLabel.Name = "CurrentSampleRateLabel"
+        Me.CurrentSampleRateLabel.Size = New System.Drawing.Size(131, 16)
+        Me.CurrentSampleRateLabel.TabIndex = 12
+        Me.CurrentSampleRateLabel.Text = "Current Sample Rate"
+        '
+        'PortComboBox
+        '
+        Me.PortComboBox.FormattingEnabled = True
+        Me.PortComboBox.Location = New System.Drawing.Point(351, 329)
+        Me.PortComboBox.Name = "PortComboBox"
+        Me.PortComboBox.Size = New System.Drawing.Size(292, 24)
+        Me.PortComboBox.TabIndex = 15
+        '
         'DataLogging
         '
-        Me.AcceptButton = Me.StartButton
+        Me.AcceptButton = Me.LogButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.StopButton
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.MaxSampleLabel)
-        Me.Controls.Add(Me.MinSampleLabel)
-        Me.Controls.Add(Me.TrackBar1)
-        Me.Controls.Add(Me.SampleRateLabel)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.ClientSize = New System.Drawing.Size(942, 474)
+        Me.Controls.Add(Me.PortComboBox)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.FileStatusLabel)
+        Me.Controls.Add(Me.MenuStrip1)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.TopMenuStrip)
-        Me.MainMenuStrip = Me.TopMenuStrip
+        Me.Controls.Add(Me.LogPictureBox)
+        Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "DataLogging"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Data Logger"
-        Me.TopMenuStrip.ResumeLayout(False)
-        Me.TopMenuStrip.PerformLayout()
         Me.GroupBox1.ResumeLayout(False)
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LogPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SampleRateTrackBar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents TopMenuStrip As MenuStrip
-    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
-    Friend WithEvents StartButton As Button
+    Friend WithEvents ContextMenuStrip As ContextMenuStrip
+    Friend WithEvents LogButton As Button
     Friend WithEvents StopButton As Button
     Friend WithEvents SaveButton As Button
     Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents StartToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents StopToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
-    Friend WithEvents InfoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LogPictureBox As PictureBox
     Friend WithEvents SerialPort As IO.Ports.SerialPort
     Friend WithEvents SampleRateLabel As Label
-    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents SampleRateTrackBar As TrackBar
     Friend WithEvents MinSampleLabel As Label
     Friend WithEvents MaxSampleLabel As Label
+    Friend WithEvents Timer As Timer
+    Friend WithEvents ToolTip As ToolTip
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents OpenFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FileStatusLabel As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents CurrentSampleRateLabel As Label
+    Friend WithEvents ComButton As Button
+    Friend WithEvents PortComboBox As ComboBox
 End Class
--- DataLogging/DataLogging.vb
+++ DataLogging/DataLogging.vb
@@ -1,6 +1,196 @@
-﻿Public Class DataLogging
+﻿Option Strict On
+Option Explicit On
+Imports System.IO.Ports
+Imports System.Threading.Thread
+Imports System.Windows.Forms.ComponentModel.Com2Interop
+Imports System.Runtime.InteropServices
+Public Class DataLogging
+
+    Dim dataQ As New Queue
+    Sub SetDefaults()
+        LogPictureBox.BackColor = Color.Black
+        SampleRateTrackBar.Value = 10
+        SampleRateTrackBar.Minimum = 1
+        SampleRateTrackBar.Maximum = 100
+    End Sub
+
+    Sub Updategraph()
+        Dim plotdata(100) As Integer
+        LogPictureBox.Refresh()
+        dataQ.CopyTo(plotdata, 0)
+        Plot(plotdata)
+
+    End Sub
+    Sub Plot(plotData() As Integer)
+        Dim g As Graphics = LogPictureBox.CreateGraphics
+        Dim pen As New Pen(Color.Lime)
+        Dim oldX%, oldY%
+        g.ScaleTransform(CSng(LogPictureBox.Width / (255 + 30)), CSng(LogPictureBox.Height / 255))
+        g.TranslateTransform(0, 255)
+        oldY = plotData(0)
+        For x = 0 To UBound(plotData) - 1
+            g.DrawLine(pen, oldX, oldY, x, plotData(x) * -1)
+            oldX = x
+            oldY = plotData(x) * -1
+        Next
+
+    End Sub
+    Sub GetNewData()
+        Dim newData As Integer
+        Dim coinToss As Integer
+        Static lastData As Integer
+        Dim data(1) As Byte
+
+        Try
+            Qy_ReadAnalogInPutA1()
+
+            Sleep(5)
+            Console.WriteLine(SerialPort.BytesToRead)
+            Dim qyBoardData(SerialPort.BytesToRead) As Byte
+            SerialPort.Read(qyBoardData, 0, SerialPort.BytesToRead)
+
+            Console.WriteLine($"High: {Hex(qyBoardData(0))} | Jelly Beans: {qyBoardData(0)}")
+            Console.WriteLine($"Low: {Hex(qyBoardData(1))}")
+
+            newData = qyBoardData(0)
+
+            'new data may be posative or negative
+            If coinToss >= 5 Then
+                newData = newData * -1
+            End If
+
+            lastData += newData
+            If lastData > 255 Then
+                lastData = 255
+            ElseIf lastData < 0 Then
+                lastData = 0
+            End If
+
+            Me.dataQ.Enqueue(lastData)
+            data(0) = CByte(lastData)
+
+            StoreData("RND", data)
+
+            'this keeps the queue limited to size of 100
+            If Me.dataQ.Count > 100 Then
+                Me.dataQ.Dequeue()
+            End If
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Sub StoreData(prefix As String, data As Byte())
+        Dim filename As String = $"log_{DateTime.Now.ToString("yyMMddhh")}.log"
+
+        FileOpen(1, filename, OpenMode.Append)
+        Write(1, $"$${prefix}")
+        Write(1, data(0))
+        Write(1, data(1))
+        WriteLine(1, $"{DateTime.Now.ToString("yyMMddhhmmss")}{DateTime.Now.Millisecond}")
+
+        FileClose(1)
+
+    End Sub
+
+    ''' <summary>
+    ''' returns an integer from min to max inclusive.
+    ''' <br/>
+    ''' defaults:
+    ''' <br/>
+    ''' min = 0
+    ''' <br/>
+    ''' max = 10
+    ''' </summary>
+    ''' <param name="min%"></param>
+    ''' <param name="max%"></param>
+    ''' <returns></returns>
+    Function RandomNumberFrom(Optional min% = 0, Optional max% = 10) As Integer
+        Dim _random%
+        Randomize(DateTime.Now.Millisecond)
+        _random = CInt(Math.Floor(Rnd() * (max + 1 - min))) + min
+        Return _random
+    End Function
+    Function Qy_ReadAnalogInPutA1() As Byte()
+        Try
+            Dim data(0) As Byte
+            data(0) = &B1010001
+            SerialPort.Write(data, 0, 1)
+            Return data
+        Catch ex As Exception
+
+        End Try
+
+    End Function
+    Sub SerialConnect(portName As String)
+        SerialPort.Close()
+        SerialPort.PortName = portName
+        SerialPort.BaudRate = 9600
+        SerialPort.Open()
+    End Sub
+    Sub GetPorts()
+        'add all available ports to the port combobox
+        PortComboBox.Items.Clear()
+        For Each s As String In SerialPort.GetPortNames()
+            PortComboBox.Items.Add($"{s}")
+        Next
+
+        PortComboBox.SelectedIndex = 0
+    End Sub
+    Private Sub ComButton_Click(sender As Object, e As EventArgs) Handles ComButton.Click
+        GetPorts()
+    End Sub
+    Private Sub PortComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles PortComboBox.SelectedIndexChanged
+        SerialConnect($"{PortComboBox.SelectedItem}")
+    End Sub
+
+    ' Events below here ---------------------------------------------------------------
+    Private Sub LoggingForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetDefaults()
+        For i = 0 To 100
+            GetNewData()
+        Next
+        GetPorts()
+
+    End Sub
+
+    'Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenFileToolStripMenuItem.Click
+    '    'filter only for .log files with all files optional
+    '    OpenFileDialog.Filter = "Log Files (*.log)|*.log|All Files (*.*)|*.*"
+    '    OpenFileDialog.FileName = ""
+    '    OpenFileDialog.ShowDialog()
+    '    FileStatusLabel.Text = OpenFileDialog.FileName 'TODO log file function
+    'End Sub
+
+    'Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+    '    'filter only for .log files with all files optional
+    '    SaveFileDialog.Filter = "Log Files (*.log)|*.log|All Files (*.*)|*.*"
+    '    SaveFileDialog.FileName = $"data_{DateTime.Now.ToString("yyMMddhh")}.log"
+    '    SaveFileDialog.ShowDialog()
+    'End Sub
+
+
+    Private Sub LogButton_Click(sender As Object, e As EventArgs) Handles LogButton.Click
+
+        If Timer.Enabled Then
+            Timer.Enabled = False
+        Else
+            Timer.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        GetNewData()
+        Updategraph()
+
+    End Sub
     Private Sub StopButton_Click(sender As Object, e As EventArgs) Handles StopButton.Click
         Me.Close()
     End Sub
 
+    Private Sub SampleRateTrackBar_Scroll(sender As Object, e As EventArgs) Handles SampleRateTrackBar.Scroll
+        Timer.Interval = CInt(1000 / SampleRateTrackBar.Value)
+        CurrentSampleRateLabel.Text = $"{SampleRateTrackBar.Value} samples / sec"
+    End Sub
 End Class
